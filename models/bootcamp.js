55ed2d6a502f86f339cfc0178965e7f15451e2c5
srikanthsree50 <srikanthsree50@gmail.com>
2021-11-01 15:16:23 +0530

    geocoder

--- models/bootcamp.js
+++ models/bootcamp.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const {Schema} = require('mongoose');
+const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new Schema({
 name:{
@@ -62,6 +64,7 @@ careers:{
         'Web Development',
         'Mobile Development',
         'UI/UX',
+        "Js",
         'Data Science',
         'Business',
         'Other'
@@ -98,5 +101,26 @@ createdAt:{
     default:Date.now
 }
 });
+
+BootcampSchema.pre('save',  function(next) {
+    this.slug = slugify(this.name,{lower:true})
+    next();
+})
+
+BootcampSchema.pre('save', async function(next){
+    const loc = await geocoder.geocode(this.address);
+    this.location = {
+        type:'Point',
+        coordinates: [loc[0].longitude, loc[0].latitude],
+        formattedAddress: loc[0].formattedAddress,
+        street: loc[0].streetName,
+        city:loc[0].city,
+        state:loc[0].stateCode,
+        zipcode: loc[0].zipcode,
+        country: loc[0].countryCode
+    };
+    this.address = undefined;
+    next();
+})
 
 module.exports = mongoose.model('Bootcamp',BootcampSchema);
