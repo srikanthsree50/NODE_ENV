c6036ee423f5c967e06b5e2e281fe7c4103a0c2c
srikanthsree50 <srikanthsree50@gmail.com>
2021-11-23 19:57:32 +0530

    Adding Pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -28,11 +28,37 @@ if(req.query.sort){
 else{ 
     query = query.sort('-createdAt')
 }
+
+const page = parseInt(req.query.page,10) || 1;
+const limit = parseInt(req.query.limit,10) || 1;
+const startIndex = (page - 1) * limit;
+const endIndex = page * limit;
+const total = await Bootcamp.countDocuments();
+
+query = query.skip(startIndex).limit(limit);
+
 const bootcamps = await query;
+
+const pagination = {};
+
+if(endIndex < total){
+    pagination.next = {
+        page: page + 1,
+        limit
+    }
+}
+
+if(startIndex > 0){
+    pagination.prev = {
+        page: page - 1,
+        limit
+    }
+}
 
         res.status(200).json({
             success:true,
             count:bootcamps.length,
+            pagination,
             data:bootcamps
     })
 })
