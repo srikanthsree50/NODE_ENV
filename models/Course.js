c3ee6ea594209eae42c8979241f268fd6252be47
srikanthsree50 <srikanthsree50@gmail.com>
2021-12-01 21:34:55 +0530

    aggregation average cost

--- models/Course.js
+++ models/Course.js
@@ -38,4 +38,36 @@ required:true
 }
 });
 
+courseSchema.statics.getAverageCost = async function(bootcampId)  {
+const obj = await this.aggregate([
+    {
+        $match:{bootcamp:bootcampId}
+    },
+    {
+        $group:{
+            _id:'$bootcamp',
+            averageCost:{ $avg : '$courseFees'}
+        }
+    }
+])
+try{
+await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+    averageCost : Math.ceil(obj[0].averageCost/10)*10
+})
+}
+catch(err){
+console.error(err)
+}
+}
+
+courseSchema.post('save', function(){
+this.constructor.getAverageCost(this.bootcamp);
+})
+
+courseSchema.pre('remove', function(){
+    
+this.constructor.getAverageCost(this.bootcamp);
+})
+
+
 module.exports = mongoose.model('Course',courseSchema);
--- seeder.js
+++ seeder.js
@@ -23,7 +23,7 @@ const courses = JSON.parse(
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
-        await Course.create(courses);
+      //  await Course.create(courses);
         console.log('data imported.... '.green.inverse);
         process.exit();
     }
