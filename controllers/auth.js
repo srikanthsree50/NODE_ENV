578ebb56a7221482188565ee6d2c8f6df4b65c73
srikanthsree50 <srikanthsree50@gmail.com>
2021-12-10 20:52:51 +0530

    adding password encrryption with jwt bcryptjs

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,14 @@ const errorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middlewares/async');
 
 exports.register = asyncHandler(async (req,res,next) => {
+    
+    const { name,email,password,role} = req.body;
+    const user = await User.create({
+        name,
+        email,
+        password,
+        role
+    });
     res.status(200).json({
         success:true
     })
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -31,6 +32,11 @@ createdAt:{
     type:Date,
     default:Date.now
 }
+})
+
+userSchema.pre('save', async function(next) {
+const salt = await bcrypt.genSalt(10);
+this.password = await bcrypt.hash(this.password,salt);
 })
 
 module.exports = mongoose.model('User',userSchema);
