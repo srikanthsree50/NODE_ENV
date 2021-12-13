5ae3a0ae24b6e1078f582035f63bb38e1d98c1d0
srikanthsree50 <srikanthsree50@gmail.com>
2021-12-13 21:51:53 +0530

    user login jwt sign

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 const errorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middlewares/async');
+const ErrorResponse = require('../utils/errorResponse');
 
 exports.register = asyncHandler(async (req,res,next) => {
     
@@ -12,6 +13,32 @@ exports.register = asyncHandler(async (req,res,next) => {
         role
     });
 
+const token = user.getSignedJwtToken()
+
+    res.status(200).json({
+        success:true,
+        token
+    })
+}) 
+
+
+exports.login = asyncHandler(async (req,res,next) => {
+    
+    const { email,password} = req.body;
+
+    if(!email || !password){
+        return next(new ErrorResponse('please provide a valid email and password...',400));
+    }
+
+    const user = User.findOne({email}).select('+password');
+if(!user){
+    return next(new ErrorResponse('Invalid Credentials...',401));
+}
+//console.log('//user////',user)
+const isMatch = await user.matchPassword(password)
+if(!isMatch){
+    return next(new ErrorResponse('Invalid Credentials...',401));
+}
 const token = user.getSignedJwtToken()
 
     res.status(200).json({
--- models/User.js
+++ models/User.js
@@ -39,7 +39,13 @@ userSchema.pre('save', async function(next) {
 const salt = await bcrypt.genSalt(10);
 this.password = await bcrypt.hash(this.password,salt);
 })
+
 userSchema.methods.getSignedJwtToken = function() {
     return jwt.sign({id:this._id},process.env.JWT_SECRET,{expiresIn:process.env.JWT_EXPIRES})
 }
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword,this.password);
+}
+
 module.exports = mongoose.model('User',userSchema);
