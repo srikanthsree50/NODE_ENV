9c2faf51057476b8afb3912b03689ad0cf8a4bf8
srikanthsree50 <srikanthsree50@gmail.com>
2021-12-17 20:07:42 +0530

    sending jwt within cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -46,7 +46,7 @@ const sendTokenResponse = (user,statusCode,res) => {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
         httpOnly : true
     }
-
+ 
 if(process.env.NODE_ENV === 'production'){
     options.secure = true;
 }
